6b59c411120e5310ac4b0fb09c1902e780b3a61d
taralynnemcneil <taralynnemcneil@gmail.com>
2016-02-03 21:17:45 -0500

    edit

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,15 +9,15 @@ var isHungry = true;
 // arrays
 
 var usernames = ["Mike", "Rich", "Anju", "Margot"];
-console.log("Before the push" + usernames);
+console.log("Before the push", + usernames);
 
 // add and element to the bottom of the array
 usernames.push("Tom");
-console.log("After the push" + usernames);
+console.log("After the push", + usernames);
 
 // remove and element to the bottom of the array
 usernames.pop();
-console.log("After the pop" + usernames);
+console.log("After the pop", + usernames);
 
 /* ***** This is another (longer) way to create an array *****
 var usernames = [];
